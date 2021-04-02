18e76c8c4e038fb902bbb02b9cbc2459a89acbc9
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2021-04-02 17:32:32 +0100

    Added blend animations for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    
+    public Rigidbody2D Rigidbody2D;
+    public float MoveSpeed;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * MoveSpeed;
     }
 }
